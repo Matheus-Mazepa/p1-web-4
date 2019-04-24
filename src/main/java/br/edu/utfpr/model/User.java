0ab971576077534368c5f9bf63aebe13070a7012
Hythan <hythan_mco@hotmail.com>
2019-04-24 18:50:06 -0300

    com bug

---
+++ src/main/java/br/edu/utfpr/model/User.java
@@ -0,0 +1,73 @@
+package br.edu.utfpr.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name= "user_name")
+	private String userName;
+	
+	@Column(name = "password")
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	public User(int id, String name, String userName, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
